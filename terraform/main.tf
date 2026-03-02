d151484f7333ef0abd785b901b3da2850695e2fb
Sean <162363+seanezell@users.noreply.github.com>
2026-03-01 21:10:51 -0800

    fix: change user search to query, build 2nd GSI

--- terraform/main.tf
+++ terraform/main.tf
@@ -336,10 +336,27 @@ resource "aws_dynamodb_table" "what2play" {
         type = "S"
     }
 
+    attribute {
+        name = "GSI2PK"
+        type = "S"
+    }
+
+    attribute {
+        name = "GSI2SK"
+        type = "S"
+    }
+
     global_secondary_index {
         name     = "GSI1"
         hash_key = "GSI1PK"
         range_key = "GSI1SK"
+        projection_type = "ALL"
+    }
+
+    global_secondary_index {
+        name     = "GSI2"
+        hash_key = "GSI2PK"
+        range_key = "GSI2SK"
         projection_type = "ALL"
     }
 }
